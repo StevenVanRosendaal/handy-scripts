68781ea3f25ceec3fe57ad94b1f4bf63a62f3184
Steven van Rosendaal <svanrosendaal@hotmail.com>
2024-06-05 11:39:28 +0200

    added a missing -y flag to apt-get

--- steros_installer.sh
+++ steros_installer.sh
@@ -229,7 +229,7 @@ if $multi_site; then
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     tee /etc/apt/sources.list.d/docker.list > /dev/null
     apt-get update
-    apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
     usermod -aG docker $username
 
     echo "Installing Portainer..."
